77d7f518ceef19f2aed1764741e4ea83d6973875
fahamkhan00 <faham@student.iul.ac.in>
2025-07-15 00:03:59 +0530

    Test2

--- src/test/java/com/bank/mybank/MyBankApplicationTests.java
+++ src/test/java/com/bank/mybank/MyBankApplicationTests.java
@@ -2,8 +2,10 @@ package com.bank.mybank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyBankApplicationTests {
 
 	@Test
